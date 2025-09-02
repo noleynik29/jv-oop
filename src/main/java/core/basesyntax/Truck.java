7a4dc3c8a07c20deeb0afdbe5c539f7a18d0b454
noleynik29 <noleynik29@gmail.com>
2025-09-02 10:33:28 +0300

    Created class Machine containing methods public abstract void doWork() and public abstract void stopWork(). Created classes Truck, Bulldozer, and Excavator that have inherited from Machine. In those classes I have overrided doWork(), so it will print a message that a certain machine started its work. I have overrided stopWork() as well. It should print messages that certain machines stopped working. In the MainApp class created a Machine array with Truck, Bulldozer, and Excavator and called methods doWork() and stopWork() in a loop.

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -4,11 +4,11 @@ public class Truck extends Machine{
 
     @Override
     public void doWork() {
-        System.out.println("Truck is working");
+        System.out.println("Truck started working");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck is stopped");
+        System.out.println("Truck stopped working");
     }
 }
